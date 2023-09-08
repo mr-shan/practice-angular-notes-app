48fc712cbd0ba86b84f6689f57d646191c7365d3
Shantanu <kulkarni.shantanu4@gmail.com>
2023-09-08 12:19:21 +0530

    Color fixes

--- src/app/notes/note-item/note-item.component.ts
+++ src/app/notes/note-item/note-item.component.ts
@@ -1,19 +1,28 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 
 import { Note } from '../note.model';
 
 @Component({
   selector: 'app-note-item',
   templateUrl: './note-item.component.html',
-  styleUrls: ['./note-item.component.css']
+  styleUrls: ['./note-item.component.css'],
 })
 export class NoteItemComponent {
+  @ViewChild('container') container: ElementRef;
   @Input('data') data: Note = {} as Note;
 
   @Output('openNote') openNote = {} as EventEmitter<string>;
 
   constructor() {
     this.openNote = new EventEmitter();
+    this.container = {} as ElementRef;
   }
 
   onNoteClick() {
