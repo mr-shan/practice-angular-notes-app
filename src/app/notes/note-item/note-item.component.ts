f665a31420e868e293b4b20406e99f252ca3af30
Shantanu <kulkarni.shantanu4@gmail.com>
2023-09-08 12:59:55 +0530

    Worked on animating note opening

--- src/app/notes/note-item/note-item.component.ts
+++ src/app/notes/note-item/note-item.component.ts
@@ -15,17 +15,53 @@ import { Note } from '../note.model';
   styleUrls: ['./note-item.component.css'],
 })
 export class NoteItemComponent {
-  @ViewChild('container') container: ElementRef;
+  @ViewChild('containerRef') containerRef: ElementRef;
   @Input('data') data: Note = {} as Note;
 
   @Output('openNote') openNote = {} as EventEmitter<string>;
 
   constructor() {
     this.openNote = new EventEmitter();
-    this.container = {} as ElementRef;
+    this.containerRef = {} as ElementRef;
   }
 
   onNoteClick() {
+    const boundingBox = this.containerRef.nativeElement.getBoundingClientRect();
+    this.containerRef.nativeElement.classList.add('expand-note');
+    this.setBeforeExpandPositions(boundingBox);
+    
+    setTimeout(() => {
+      this.containerRef.nativeElement.classList.add('expand-note-final');
+    }, 0);
+    setTimeout(() => {
+      this.containerRef.nativeElement.classList.add('expand-note-after');
+    }, 0);
+    
+    
+    setTimeout(() => {
+      this.containerRef.nativeElement.classList.remove('expand-note-after');
+      this.containerRef.nativeElement.classList.remove('expand-note-final');
+      this.containerRef.nativeElement.classList.remove('expand-note');
+      this.resetContainerPositions();
+    }, 50);
+
+
     this.openNote.emit(this.data.id);
+  }
+
+  setBeforeExpandPositions(box: DOMRect) {
+    this.containerRef.nativeElement.style.width = box.width + 'px';
+    this.containerRef.nativeElement.style.height = box.height + 'px';
+    this.containerRef.nativeElement.style.left = box.left + 'px';
+    this.containerRef.nativeElement.style.right = box.right + 'px';
+    this.containerRef.nativeElement.style.top = box.top + 'px';
+  }
+
+  resetContainerPositions() {
+    this.containerRef.nativeElement.style.width = '';
+    this.containerRef.nativeElement.style.height = '';
+    this.containerRef.nativeElement.style.left = '';
+    this.containerRef.nativeElement.style.right = '';
+    this.containerRef.nativeElement.style.top = '';
   }
 }
